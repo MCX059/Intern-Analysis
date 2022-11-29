1f64e1caa802f6fa75bb7544a57fe6704dddaf6c
Maocheng Xu <43055265+MCX059@users.noreply.github.com>
2022-11-29 01:13:39 +0100

    Update BasicInfo.py

--- Compass/BasicInfo.py
+++ Compass/BasicInfo.py
@@ -39,7 +39,7 @@ class OverallHrefs:
         self._driver = webdriver.Chrome(options=option)
         self._driver.get(self._url)
         self._db = pymysql.connect(
-            'localhost', 'root', 'epaiiplus1E0', 'zhinanzhe')
+            'localhost', 'root', '#####', 'zhinanzhe')
     
     def get_ids(self):
         return self._ids
